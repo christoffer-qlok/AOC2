76137de6135ded1033b2753cfb92041864330e82
christoffer.fjellstrom <christoffer.fjellstrom@qlok.se>
2023-12-29 19:49:16 +0100

    Add project files.

---
+++ AOC2/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace AOC2
+{
+    internal class Program
+    {
+        static Dictionary<string, int> cubes = new Dictionary<string, int>()
+            {
+                { "red", 12 },
+                { "green", 13},
+                { "blue", 14 }
+            };
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            string[] lines = File.ReadAllLines("input.txt");
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(": ", 2);
+                int id = int.Parse(parts[0].Split(' ', 2)[1]);
+                string[] drawLines = parts[1].Split("; ");
+                string[][] draws = new string[drawLines.Length][];
+                for (int i = 0; i < drawLines.Length; i++)
+                {
+                    draws[i] = drawLines[i].Split(", ");
+                }
+
+                if(CheckDraws(draws))
+                {
+                    sum += id;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+
+        static bool CheckDraws(string[][] draws)
+        {
+            return draws.All(d => CheckSingleDraw(d));
+        }
+
+        static bool CheckSingleDraw(string[] draw)
+        {
+            foreach (string set in draw)
+            {
+                var parts = set.Split(" ", 2);
+                var count = int.Parse(parts[0]);
+                if(count > cubes[parts[1]])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Part2
+{
+    internal class Program
+    {
+        static Dictionary<string, int> cubes = new Dictionary<string, int>()
+            {
+                { "red", 12 },
+                { "green", 13},
+                { "blue", 14 }
+            };
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            string[] lines = File.ReadAllLines("input.txt");
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(": ", 2);
+                int id = int.Parse(parts[0].Split(' ', 2)[1]);
+                string[] drawLines = parts[1].Split("; ");
+                string[][] draws = new string[drawLines.Length][];
+                for (int i = 0; i < drawLines.Length; i++)
+                {
+                    draws[i] = drawLines[i].Split(", ");
+                }
+
+                sum += GetPower(draws);
+            }
+            Console.WriteLine(sum);
+        }
+
+        static int GetPower(string[][] draws)
+        {
+            var minCubes = new Dictionary<string, int>()
+            {
+                { "red", 0 },
+                { "green", 0},
+                { "blue", 0 }
+            };
+            foreach (var draw in draws)
+            {
+                foreach (string set in draw)
+                {
+                    var parts = set.Split(" ", 2);
+                    var count = int.Parse(parts[0]);
+                    var color = parts[1];
+                    minCubes[color] = Math.Max(minCubes[color], count);
+                }
+            }
+            return minCubes["red"] * minCubes["blue"] * minCubes["green"];
+        }
+    }
+}
